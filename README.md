2a4a4357ea7d2b37f897b0bbed4732fa0c76ebe3
archibate <1931127624@qq.com>
2023-06-23 16:50:23 +0800

    adderr

---
+++ README.md
@@ -0,0 +1,8 @@
+# 现代 OpenGL 保姆级教程
+
+零基础入门计算机图形学必不可少的在线网络公开课，手把手教您现代 OpenGL 的点点滴滴，构建爆款游戏引擎。
+
+本仓库包含实验源码和 PPT 课件。
+
+国外下载：https://github.com/archibate/opengltutor
+国内下载：https://gitee.com/archibate/opengltutor
--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,71 @@
 #include <GLFW/glfw3.h>
 #include "Game.hpp"
 #include <stdexcept>
-#include <string>
-#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 
 int main() {
     if (!glfwInit()) {
-        throw std::runtime_error("failed to initialize GLFW");
+        const char *errmsg;
+        glfwGetError(&errmsg);
+        if (!errmsg) errmsg = "(no error)";
+        std::cerr << "failed to initialize GLFW: " << errmsg << '\n';
+        return -1;
     }
-#ifdef FULLSCRN
-    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
-    const GLFWvidmode *mode = glfwGetVideoMode(monitor);
-    GLFWwindow *window = glfwCreateWindow(mode->width, mode->height, "Example", NULL, NULL);
-    glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
-#else
+    // OpenGL 2.0, Compatibile Profile
+    glfwWindowHint(GLFW_OPENGL_API, GLFW_OPENGL_API);
+    /* glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE); */
+    glfwWindowHint(GLFW_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_VERSION_MINOR, 0);
+    // Create window
     GLFWwindow *window = glfwCreateWindow(640, 640, "Example", NULL, NULL);
-#endif
     if (!window) {
         glfwTerminate();
         const char *errmsg;
         glfwGetError(&errmsg);
-        throw std::runtime_error("GLFW failed to create window: "
-                                 + std::string(errmsg));
+        if (!errmsg) errmsg = "(no error)";
+        std::cerr << "GLFW failed to create window: " << errmsg << '\n';
+        std::cerr << "==============================================\n";
+        if (!strcmp(errmsg, "X11: The DISPLAY environment variable is missing")) {
+            std::cerr << "You seems not running with graphic display\n";
+        } else if (!strcmp(errmsg, "WGL: The driver does not appear to support OpenGL")) {
+            std::cerr << "Please consider install an OpenGL driver, or use the mesa driver\n";
+        }
+        std::cerr << "- If you have a physical graphic card (e.g. NVIDIA), install it from your graphic card vendor official website: http://www.nvidia.com/Download/index.aspx\n";
+        std::cerr << "- If you are using Windows, install the mesa driver: https://github.com/pal1000/mesa-dist-win\n";
+        std::cerr << "- If you are using Linux or WSL1, install the mesa driver: https://ubuntuhandbook.org/index.php/2021/07/install-latest-mesa-ubuntu-20-04-21-04/";
+        std::cerr << "- If you use WSL2, install WSLg: https://learn.microsoft.com/zh-cn/windows/wsl/tutorials/gui-apps\n";
+        std::cerr << "- If you are using SSH remote server, try connect it using ssh -X <ip address>\n";
+        std::cerr << "- Also make sure you have switch to dedicated card instead of the integrated card\n";
+        std::cerr << "==============================================\n";
+#ifdef _WIN32
+        std::system("pause");
+#endif
+        return -1;
     }
     glfwMakeContextCurrent(window);
 
+    // Load glXXX function pointers
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         glfwTerminate();
-        throw std::runtime_error("GLAD failed to load GL functions");
+        std::cerr << "GLAD failed to load GL functions\n";
+        return -1;
     }
-    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+    std::cerr << "OpenGL version: " << glGetString(GL_VERSION) << '\n';
 
+    // start main game loop
     {
         Game game;
         double t0 = glfwGetTime();
         while (!glfwWindowShouldClose(window)) {
+            // clean screen
             /* CHECK_GL(glClear(GL_COLOR_BUFFER_BIT)); */
+            // game world move forward
             double t1 = glfwGetTime();
             game.tick(t1 - t0);
             t0 = t1;
+            // refresh screen
             glfwSwapBuffers(window);
             glfwPollEvents();
         }

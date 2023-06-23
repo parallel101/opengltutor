6dd9cff8d1929798476ae26830afe24c7bfb8ab3
archibate <1931127624@qq.com>
2023-06-23 20:35:02 +0800

    add file ignore

--- include/GLRAII.hpp
+++ include/GLRAII.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include "check_gl.hpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
-#include "check_gl.hpp"
-#include <GLFW/glfw3.h>
-#include "Game.hpp"
+#include "check_gl.hpp" // includes glad/glad.h
+#include <GLFW/glfw3.h> // must be placed behind glad/glad.h
 #include <stdexcept>
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+
+static void render() {
+    CHECK_GL(glClear(GL_COLOR_BUFFER_BIT));
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(0.0f, 0.5f, 0.0f);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, 0.0f);
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.5f, -0.5f, 0.0f);
+    CHECK_GL(glEnd());
+}
 
 int main() {
     if (!glfwInit()) {
@@ -16,15 +27,22 @@ int main() {
         std::cerr << "failed to initialize GLFW: " << errmsg << '\n';
         return -1;
     }
-    // OpenGL 2.0, Compatibile Profile
+
+    // hint the version required: OpenGL 2.0
+    constexpr int version = 20;
     glfwWindowHint(GLFW_OPENGL_API, GLFW_OPENGL_API);
-    /* glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE); */
-    glfwWindowHint(GLFW_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, version / 10);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, version % 10);
+    if (version >= 33) {
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    }
+
     // Create window
     GLFWwindow *window = glfwCreateWindow(640, 640, "Example", NULL, NULL);
     if (!window) {
-        glfwTerminate();
         const char *errmsg;
         glfwGetError(&errmsg);
         if (!errmsg) errmsg = "(no error)";
@@ -34,17 +52,21 @@ int main() {
             std::cerr << "You seems not running with graphic display\n";
         } else if (!strcmp(errmsg, "WGL: The driver does not appear to support OpenGL")) {
             std::cerr << "Please consider install an OpenGL driver, or use the mesa driver\n";
+        } else if (!strcmp(errmsg, "WGL: Failed to create OpenGL context")) {
+            std::cerr << "Your driver seems not supporting the required OpenGL version\n";
         }
         std::cerr << "- If you have a physical graphic card (e.g. NVIDIA), install it from your graphic card vendor official website: http://www.nvidia.com/Download/index.aspx\n";
-        std::cerr << "- If you are using Windows, install the mesa driver: https://github.com/pal1000/mesa-dist-win\n";
+        std::cerr << "- If you are using Windows, download opengl32.dll from https://pan.baidu.com/s/1TZ6nVJC7DZIuUarZrGJYow?pwd=opgl and place it into the same directory as this executable file (alternatively you may download opengl32sw.dll from Internet and rename it to opengl32.dll to place into the same directory as this executable file)\n";
         std::cerr << "- If you are using Linux or WSL1, install the mesa driver: https://ubuntuhandbook.org/index.php/2021/07/install-latest-mesa-ubuntu-20-04-21-04/";
         std::cerr << "- If you use WSL2, install WSLg: https://learn.microsoft.com/zh-cn/windows/wsl/tutorials/gui-apps\n";
         std::cerr << "- If you are using SSH remote server, try connect it using ssh -X <ip address>\n";
-        std::cerr << "- Also make sure you have switch to dedicated card instead of the integrated card\n";
+        std::cerr << "- If you are using MacOS, you probably want to use Windows or Linux instead for better OpenGL support\n";
+        std::cerr << "- If you are using a Laptop with dual-cards, make sure you have switch to dedicated card (NVIDIA) instead of the integrated card (Intel)\n";
         std::cerr << "==============================================\n";
 #ifdef _WIN32
         std::system("pause");
 #endif
+        glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
@@ -58,20 +80,13 @@ int main() {
     std::cerr << "OpenGL version: " << glGetString(GL_VERSION) << '\n';
 
     // start main game loop
-    {
-        Game game;
-        double t0 = glfwGetTime();
-        while (!glfwWindowShouldClose(window)) {
-            // clean screen
-            /* CHECK_GL(glClear(GL_COLOR_BUFFER_BIT)); */
-            // game world move forward
-            double t1 = glfwGetTime();
-            game.tick(t1 - t0);
-            t0 = t1;
-            // refresh screen
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-        }
+    double t0 = glfwGetTime();
+    while (!glfwWindowShouldClose(window)) {
+        // render graphics
+        render();
+        // refresh screen
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
     glfwTerminate();

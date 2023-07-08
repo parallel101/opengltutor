b56c4f6e63c8e778c480b8bc6e6f8df0ae7bb6b8
archibate <1931127624@qq.com>
2023-07-08 18:12:21 +0800

    add assets

--- src/main.cpp
+++ src/main.cpp
@@ -10,60 +10,61 @@
 #include <sstream>
 #include <vector>
 
-struct Vertex {
-    float x, y, z;
-};
+static std::vector<glm::vec3> vertices;
+static std::vector<glm::uvec3> faces;
 
-struct Face {
-    unsigned int a, b, c;
-};
+static void load_obj(std::string path) {
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        std::cerr << "Failed to open file: " << path << '\n';
+        return;
+    }
 
-static std::vector<Vertex> vertices = {
-    {-0.5f, 0.5f, 0.0f},
-    {-0.5f, -0.5f, 0.0f},
-    {0.5f, -0.5f, 0.0f},
-    {0.5f, 0.5f, 0.0f},
-};
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.substr(0, 2) == "v ") {
+            std::istringstream s(line.substr(2));
+            glm::vec3 vertex;
+            s >> vertex.x >> vertex.y >> vertex.z;
+            vertices.push_back(vertex);
+        } else if (line.substr(0, 2) == "f ") {
+           std::istringstream s(line.substr(2));
+           std::string splitted;
+           std::vector<unsigned int> indices;
+           while (std::getline(s, splitted, ' ')) {
+               unsigned int index;
+               std::istringstream(splitted) >> index;
+               indices.push_back(index - 1);
+           }
+           for (size_t i = 2; i < indices.size(); i++) {
+               glm::uvec3 face = {indices[0], indices[i - 1], indices[i]};
+               faces.push_back(face);
+           }
+       }
+    }
 
-static std::vector<Face> faces = {
-    {0, 1, 2},
-    {0, 2, 3},
-};
+    file.close();
+    std::cout << "Loaded " << vertices.size() << " vertices, " << faces.size() << " faces.\n";
+}
 
-/* static void load_obj(std::string path) { */
-/*     std::ifstream file(path); */
-/*     if (!file.is_open()) { */
-/*         std::cerr << "Failed to open file: " << path << '\n'; */
-/*         return; */
-/*     } */
-/*  */
-/*     std::string line; */
-/*     while (std::getline(file, line)) { */
-/*         if (line.substr(0, 2) == "v ") { */
-/*             std::istringstream s(line.substr(2)); */
-/*             Vertex vertex; */
-/*             s >> vertex.x >> vertex.y >> vertex.z; */
-/*             vertices.push_back(vertex); */
-/*         } */
-/*     } */
-/*  */
-/*     file.close(); */
-/*     std::cout << "Loaded " << vertices.size() << " vertices.\n"; */
-/* } */
-
-static void render() {
+static void draw_obj() {
     glBegin(GL_TRIANGLES);
 
     for (auto const &face : faces) {
-        Vertex a = vertices[face.a];
-        Vertex b = vertices[face.b];
-        Vertex c = vertices[face.c];
-        glVertex3f(a.x, a.y, a.z);
-        glVertex3f(b.x, b.y, b.z);
-        glVertex3f(c.x, c.y, c.z);
+        auto const &a = vertices.at(face.x);
+        auto const &b = vertices.at(face.y);
+        auto const &c = vertices.at(face.z);
+
+        glVertex3fv(glm::value_ptr(a));
+        glVertex3fv(glm::value_ptr(b));
+        glVertex3fv(glm::value_ptr(c));
     }
 
     CHECK_GL(glEnd());
+}
+
+static void render() {
+    draw_obj();
 }
 
 static void mouse_button_callback
@@ -83,12 +84,12 @@ static void mouse_button_callback
         float x = (float)(2 * xpos / width - 1);
         float y = (float)(2 * (height - ypos) / height - 1);
 
-        vertices[0] = Vertex{x, y, 0};
+        /* camera_on_lmb(x, y); */
     }
 }
 
 static void initialize() {
-    /* load_obj("/home/bate/Codes/zeno_assets/assets/monkey.obj"); */
+    load_obj("/home/bate/Codes/zeno_assets/assets/monkey.obj");
     CHECK_GL(glEnable(GL_DEPTH_TEST));
 }
 

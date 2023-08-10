5b9bd6ceb0e6dc78bc7dbecc06be7a26fee962dc
crowiidoka <1951438932@qq.com>
2023-08-10 21:21:31 +0800

    UTF-8（试试）

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#pragma execution_character_set("utf-8")  
+#pragma execution_character_set("utf-8") 
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include "check_gl.hpp" // includes glad/glad.h
